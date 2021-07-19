b8c6f8006c13d5ab940fe48bce2c6918690917b7
Bruno Bulnes <bulnes.bruno@gmail.com>
2021-07-19 19:35:50 -0300

    task: crossorigin attr

--- pages/[slug].js
+++ pages/[slug].js
@@ -13,7 +13,7 @@ export default function Article({ post }) {
         <link href="/favicon.png" rel="shortcut icon" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet"></link>
       </Head>
 
--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home({ posts }) {
         <link href="/favicon.png" rel="shortcut icon" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet"></link>
       </Head>
 
